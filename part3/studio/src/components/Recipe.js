b6a0b5713a02527364aa50c0686e81d9a89a5c6f
Greg <138933807+sinistergreg@users.noreply.github.com>
2023-08-10 14:39:42 -0600

    React Part 3 Studio

--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,24 +1,25 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://www.gordonramsay.com/gr/recipes/";
+   let authorPhoto = "https://www.gordonramsay.com/assets/Uploads/_resampled/CroppedFocusedImage160055050-50-GR-MGPW.jpg";
+   let authorName = "Gordon Ramsay";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "Gordon Ramsay" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink}>Gordon Ramsay's Website</a> 
          </div>
       </div>
    );
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["Free-range turkey", "Sea salt", "Freshly ground pepper", "Onions", "Lemon"];
    return(
       <div>
-         <h3>Recipe Ingredients</h3>
+         <h3>ROAST TURKEY WITH LEMON, PARSLEY, & GARLIC</h3>
+            <h4><u>Recipe Ingredients</u></h4>
          <ul>
             <li>{ingredients[0]}</li>
             <li>{ingredients[1]}</li>
@@ -26,16 +27,15 @@ const RecipeIngredients = () => {
             <li>{ingredients[3]}</li>
             <li>{ingredients[4]}</li>
          </ul>
+         <h5> <img src='https://www.gordonramsay.com/assets/Recipes/_resampled/CroppedFocusedImage192072050-50-Gordon-Carving-Christmas-Turkey.jpg' alt="turkey" width="350" height="245"/> </h5>
       </div>
    );
 }
-
 const RecipeDescription = () => {
    return (
       <div> 
          <div>
             <h1></h1>
-            <p></p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
