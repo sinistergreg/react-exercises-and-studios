ae9c84b2820ceae321fc09133dc2f9efdaaad524
Greg <138933807+sinistergreg@users.noreply.github.com>
2023-08-10 11:23:43 -0600

    React Part 3 Exercise

---
+++ part3/exercise/new-app/src/components/Introduction.js
@@ -0,0 +1,31 @@
+export default function HobbyIntroduction () {
+    return(
+    <div>
+        <div>
+            <h2 className="header">3 Fun Facts About this Hobby</h2>
+
+       <p><b><u>I do my own artwork!</u></b><br></br>
+        * I have been drawing since I was 7 years old! <br></br>
+        * I was an art major in high school and college! <br></br>
+        * I created a logo for my Burr McMillen Music Page! <br></br>
+       </p>
+
+       <p><b><u>I play rhythm and lead guitar!</u></b><br></br> 
+        * I am a song writer. <br></br>
+        * I have been playing guitar for over 30 years. <br></br>
+        * I started playing guitar the summer before I went to high school  </p>
+
+       <p><b><u>I am a singer and a song writer!</u></b><br></br> 
+        * I have been in singing competitions. <br></br>
+        * I have won prizes in singing competitions, including money and an elvis statue. <br></br>
+        * I once auditioned for X-factor <br></br><br></br>
+        <b><u>Check out my music page:</u></b><br></br><br></br>
+        https://www.facebook.com/burrmcmillen/
+
+        </p>
+        
+       </div>
+       </div>
+    );
+ }
+ 
